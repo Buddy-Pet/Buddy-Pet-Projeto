d23a49d0dc59c19e44098b058541db00510ff54d
henriquelima23 <lima.lima2306@gmail.com>
2022-10-17 16:13:48 -0300

    atualizando branch

--- public/js/buscaCep.js
+++ public/js/buscaCep.js
@@ -4,20 +4,25 @@ const inputLogradouro = document.getElementsByName('logradouro')[0];
 const inputBairro = document.getElementsByName('bairro')[0];
 const inputLocalidade = document.getElementsByName('localidade')[0];
 const inputUf = document.getElementsByName('uf')[0];
-
+console.log(btnCep)
 btnCep.onclick = () => {
 	const cep = inputCep.value;
 
 	if (!cep) return
 
-fetch(`https://viacep.com.br/ws/${cep})/json/`)
-
-	.then(response => response.json())
-	.then(jsonResponse => {
-		inputLogradouro.value = jsonResponse.logradouro;
-		inputBairro.value = jsonResponse.bairro;
-		inputLocalidade.value = jsonResponse.localidade;
-		inputUf.value = jsonResponse.uf;
-	});
+	fetch(`https://viacep.com.br/ws/${cep}/json/`, {
+		// mode:"no-cors"
+		
+	})
+		
+		.then(response => response.text())
+		.then(jsonResponse  => {
+			inputLogradouro.value = jsonResponse.logradouro;
+			inputBairro.value = jsonResponse.bairro;
+			inputLocalidade.value = jsonResponse.localidade;
+			inputUf.value = jsonResponse.uf;
+			
+		})
+		.catch(error => console.log(error));
 
 };
